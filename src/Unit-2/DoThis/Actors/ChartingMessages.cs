7ad82db9aa2c9da04323449f01c66fc8f7331995
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2017-05-15 19:45:21 +0200

    lesson 2.3 completed

---
+++ src/Unit-2/DoThis/Actors/ChartingMessages.cs
@@ -0,0 +1,66 @@
+﻿using Akka.Actor;
+
+namespace ChartApp.Actors
+{
+    #region Reporting
+
+    /// <summary>
+    /// Signal used to indicate that it's time to sample all counters.
+    /// </summary>
+    public class GatherMetrics { }
+
+    /// <summary>
+    /// Metric data at the time of sample
+    /// </summary>
+    public class Metric
+    {
+        public Metric(string series, float counterValue)
+        {
+            CounterValue = counterValue;
+            Series = series;
+        }
+
+        public float CounterValue { get; }
+        public string Series { get; }
+    }
+
+    #endregion
+
+    #region Performance Counter Management
+
+    /// <summary>
+    /// All types of counters supported by this example
+    /// </summary>
+    public enum CounterType
+    {
+        Cpu,
+        Memory,
+        Disk
+    }
+
+    public class SubscribeCounter
+    {
+        public SubscribeCounter(CounterType counter, IActorRef subscriber)
+        {
+            Subscriber = subscriber;
+            Counter = counter;
+        }
+
+        public IActorRef Subscriber { get; }
+        public CounterType Counter { get; }
+    }
+
+    public class UnsubscribeCounter
+    {
+        public UnsubscribeCounter(CounterType counter, IActorRef subscriber)
+        {
+            Counter = counter;
+            Subscriber = subscriber;
+        }
+
+        public CounterType Counter { get; }
+        public IActorRef Subscriber { get; }
+    }
+
+    #endregion
+}
---
+++ src/Unit-2/DoThis/Actors/PerformanceCounterActor.cs
@@ -0,0 +1,87 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ChartApp.Actors
+{
+    /// <summary>
+    /// Actor responsible for monitoring a specific <see cref="PerformanceCounter"/>
+    /// </summary>
+    public class PerformanceCounterActor : ReceiveActor
+    {
+        private readonly string _seriesName;
+        private readonly Func<PerformanceCounter> _performanceCounterGenerator;
+        private PerformanceCounter _counter;
+
+        private readonly HashSet<IActorRef> _subscriptions;
+        private readonly ICancelable _cancelPublishing;
+
+        public PerformanceCounterActor(string seriesName, Func<PerformanceCounter> performanceCounterGenerator)
+        {
+            _seriesName = seriesName;
+            _performanceCounterGenerator = performanceCounterGenerator;
+            _subscriptions = new HashSet<IActorRef>();
+            _cancelPublishing = new Cancelable(Context.System.Scheduler);
+
+            Receive<GatherMetrics>(m => Handle(m));
+            Receive<SubscribeCounter>(m => Handle(m));
+            Receive<UnsubscribeCounter>(m => Handle(m));
+        }
+
+        #region Actor lifecycle methods
+
+        protected override void PreStart()
+        {
+            _counter = _performanceCounterGenerator();
+            Context.System.Scheduler.ScheduleTellRepeatedly(
+                TimeSpan.FromMilliseconds(250),
+                TimeSpan.FromMilliseconds(250),
+                Self,
+                new GatherMetrics(),
+                Self,
+                _cancelPublishing
+                );
+        }
+
+        protected override void PostStop()
+        {
+            try
+            {
+                // termina the scheduled task
+                _cancelPublishing.Cancel(false);
+                _counter.Dispose();
+            }
+            catch
+            {
+                // ignore exception
+            }
+            finally
+            {
+                base.PostStop();
+            }
+        }
+
+        #endregion
+
+        private void Handle(GatherMetrics message)
+        {
+            //publish latest counter value to all subscribers
+            var metric = new Metric(_seriesName, _counter.NextValue());
+            foreach (var sub in _subscriptions)
+                sub.Tell(metric);
+        }
+
+        private void Handle(SubscribeCounter message)
+        {
+            // add a subscription for this counter
+            // (it's parent's job to filter by counter types)
+            _subscriptions.Add(message.Subscriber);
+        }
+
+        private void Handle(UnsubscribeCounter message)
+        {
+            _subscriptions.Remove(message.Subscriber);
+        }
+    }
+}
