0cac44865f215a295d4046a7d4fb8bd8348b8e9b
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2017-05-18 17:22:24 +0200

    lesson 3.1 completed

--- src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
+++ src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
@@ -8,8 +8,8 @@ namespace GithubActors.Actors
     /// <summary>
     /// Top-level actor responsible for coordinating and launching repo-processing jobs
     /// </summary>
-    public class GithubCommanderActor : ReceiveActor
-    {
+    public class GithubCommanderActor : ReceiveActor, IWithUnboundedStash
+	{
         #region Message classes
 
         public class CanAcceptJob
@@ -47,36 +47,87 @@ namespace GithubActors.Actors
         private IActorRef _coordinator;
         private IActorRef _canAcceptJobSender;
 
-        public GithubCommanderActor()
+		public IStash Stash { get; set; }
+
+		private int pendingJobReplies;
+
+		public GithubCommanderActor()
+		{
+			Ready();
+		}
+
+		private void Ready()
+		{
+			Receive<CanAcceptJob>(job =>
+			{
+				_coordinator.Tell(job);
+
+				BecomeAsking();
+			});
+		}
+
+		private void BecomeAsking()
+		{
+			_canAcceptJobSender = Sender;
+			pendingJobReplies = 3; //the number of routees
+			Become(Asking);
+		}
+
+		private void Asking()
+		{
+			// stash any subsequent requests
+			Receive<CanAcceptJob>(job => Stash.Stash());
+
+			Receive<UnableToAcceptJob>(job =>
+			{
+				pendingJobReplies--;
+				if (pendingJobReplies == 0)
+				{
+					_canAcceptJobSender.Tell(job);
+					BecomeReady();
+				}
+			});
+
+			Receive<AbleToAcceptJob>(job =>
+			{
+				_canAcceptJobSender.Tell(job);
+
+				// start processing messages
+				Sender.Tell(new GithubCoordinatorActor.BeginJob(job.Repo));
+
+				// launch the new window to view results of the processing
+				Context.ActorSelection(ActorPaths.MainFormActor.Path).Tell(
+					new MainFormActor.LaunchRepoResultsWindow(job.Repo, Sender));
+
+				BecomeReady();
+			});
+		}
+
+		private void BecomeReady()
+		{
+			Become(Ready);
+			Stash.UnstashAll();
+		}
+
+		protected override void PreStart()
         {
-            Receive<CanAcceptJob>(job =>
-            {
-                _canAcceptJobSender = Sender;
-                _coordinator.Tell(job);
-            });
+			// create three GithubCoordinatorActor instances
+			var c1 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
+				ActorPaths.GithubCoordinatorActor.Name + "1");
+			var c2 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
+				ActorPaths.GithubCoordinatorActor.Name + "2");
+			var c3 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
+				ActorPaths.GithubCoordinatorActor.Name + "3");
 
-            Receive<UnableToAcceptJob>(job =>
-            {
-                _canAcceptJobSender.Tell(job);
-            });
-
-            Receive<AbleToAcceptJob>(job =>
-            {
-                _canAcceptJobSender.Tell(job);
-
-                //start processing messages
-                _coordinator.Tell(new GithubCoordinatorActor.BeginJob(job.Repo));
-
-                //launch the new window to view results of the processing
-                Context.ActorSelection(ActorPaths.MainFormActor.Path).Tell(new MainFormActor.LaunchRepoResultsWindow(job.Repo, Sender));
-            });
-        }
-
-        protected override void PreStart()
-        {
-            _coordinator = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()), ActorPaths.GithubCoordinatorActor.Name);
-            base.PreStart();
-        }
+			// create a broadcast router who will ask all of them 
+			// if they're available for work
+			_coordinator =
+				Context.ActorOf(Props.Empty.WithRouter(
+					new BroadcastGroup(ActorPaths.GithubCoordinatorActor.Path + "1",
+					ActorPaths.GithubCoordinatorActor.Path + "2",
+					ActorPaths.GithubCoordinatorActor.Path + "3")));
+			base.PreStart();
+		}
 
         protected override void PreRestart(Exception reason, object message)
         {
